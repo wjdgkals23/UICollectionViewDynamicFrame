c5bddb3c3cb07e85e1df959c8602cd4869cd6da2
wjdgkals23 <wjdgkals74@gmail.com>
2021-08-08 10:39:56 +0900

    Initial Commit

---
+++ HMDynamicCollectionViewHeightSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HMDynamicCollectionViewHeightSample
+//
+//  Created by 정하민 on 2021/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
